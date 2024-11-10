d549db05200c0e29499ebf591243d5600d216d25
Julia Terry <156032724+tuliajerry@users.noreply.github.com>
2024-11-10 12:35:04 -0600

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,16 +1,15 @@
 const { Schema, model, models } = require('mongoose');
-const bcrypt = require('bcrypt')
+const bcrypt = require('bcrypt');
 
 const UserSchema = new Schema({
-// Create a username property of type String that is required
-// Create a password property of type String that is required
-// with minimum length of 5 and max length 20
-})
+  username: { type: String, required: true },
+  password: { type: String, required: true, minlength: 5, maxlength: 20 },
+});
 
-// hashes the password before it's stored in mongo
+
 UserSchema.pre('save', async function(next) {
-  this.password = await bcrypt.hash(this.password, 10)
-  next()
-})
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
 
-module.exports = models.User || model('User', UserSchema)
+module.exports = models.User || model('User', UserSchema);
