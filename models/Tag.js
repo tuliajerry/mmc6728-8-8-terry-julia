5654a6e3b5e02f956f6e5a2ed161bba07edb4e2d
Julia Terry <156032724+tuliajerry@users.noreply.github.com>
2024-11-10 12:34:42 -0600

    Update Tag.js

--- models/Tag.js
+++ models/Tag.js
@@ -1,7 +1,7 @@
 const { Schema, model, models } = require('mongoose');
 
 const TagSchema = new Schema({
-// Create a name property with type String and make it required and unique
-})
+  name: { type: String, required: true, unique: true },
+});
 
-module.exports = models.Tag || model('Tag', TagSchema)
+module.exports = models.Tag || model('Tag', TagSchema);
