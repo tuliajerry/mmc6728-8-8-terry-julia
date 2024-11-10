1c30f97c904aaea96ae416e9498d552ddca9eb75
Julia Terry <156032724+tuliajerry@users.noreply.github.com>
2024-11-10 12:33:47 -0600

    Update Comment.js

--- models/Comment.js
+++ models/Comment.js
@@ -1,9 +1,9 @@
 const { Schema } = require('mongoose');
 
 const CommentSchema = new Schema({
-// Create an "author" property with type String and default of 'Anonymous'
-// Create a "body" property with type String and make it required
-// Create a "createdAt" property with type Date and set default to Date.now
-})
+  author: { type: String, default: 'Anonymous' },
+  body: { type: String, required: true },
+  createdAt: { type: Date, default: Date.now },
+});
 
-module.exports = CommentSchema
+module.exports = CommentSchema;
